7706d225206ec2934072a2938183ce9cc5f009b6
Geronimo Valeiras <gerovaleiras96@outlook.com>
2022-06-05 17:48:03 -0300

    Corrigiendo Entrega

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <navbar>
+        <h1>Título en proceso</h1>
+        <ul>
+            <li><a href="https://www.facebook.com" target='_blank'>Facebook</a></li>
+            <li><a href="https://www.instagram.com" target='_blank'>Instagram</a></li>
+            <li><a href="https://www.linkedin.com" target='_blank'>Linkedin</a></li>
+        </ul>
+    </navbar>
+  )
+}
+
+export default Navbar
